b190d288db8a9d0ab41266a8360621c2813663a9
carlalap <carlos3_alarcon@hotmail.com>
2023-04-12 10:54:43 -0700

    updated path

--- path.c
+++ path.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * parse_text_path - parse the input path
  * @str: string of the path
@@ -24,12 +23,10 @@ void parse_text_path(char *str, char **parsed)
 	parsed[i] = NULL;
 	free(dest);
 }
-
 /**
  * create_struct - print nodes size
  * @head: node
  * @str: string
- *
  *  Return: nodes head
  */
 paths_t *create_struct(paths_t **head, char *str)
@@ -40,15 +37,13 @@ paths_t *create_struct(paths_t **head, char *str)
 		return (NULL);
 
 	new_node->path = _strdup(str);
-
 	if (!new_node->path)
 	{
-	free(new_node);
-	return (NULL);
+		free(new_node);
+		return (NULL);
 	}
 	new_node->next = *head;
 	*head = new_node;
-
 	return (new_node);
 }
 /**
@@ -57,42 +52,3 @@ paths_t *create_struct(paths_t **head, char *str)
  *
  *   Return: string whit the path
  */
-paths_t *get_path(char **env)
-{
-	int i = 0, j = 0, num, count;
-	char **juanito;
-	char *tmp, **tmp2 = NULL;
-	paths_t *head;
-	char *comparation = "PATH";
-/*in this part, iterate inside the environment */
-	juanito = env;
-	while (juanito[i] != NULL)
-	{
-		count = 0;
-		for (j = 0; juanito[i][j]; j++)
-		{
-			if (juanito[i][j] == comparation[j] && j < 4)
-			{
-				count++;
-				/*check for a coincidence whit the path */
-				if (count == 4 && j == 3)
-					num = i;
-			}
-			else
-				count = 0;
-		}
-		i++;
-	}
-	tmp = _strdup(juanito[num]);
-	tmp2 = malloc(sizeof(char *) * 1024);
-	if (!tmp2)
-		return (NULL);
-	/*send the coincidence to another function for tokens*/
-	parse_text_path(tmp, tmp2);
-	head = NULL;
-	for (i = 0; tmp2[i]; i++)
-		create_struct(&head, tmp2[i]);
-	free(tmp);
-	free(tmp2);
-	return (head);
-}
